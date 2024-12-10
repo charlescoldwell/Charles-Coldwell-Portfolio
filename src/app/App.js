b0e0d08abbf27374b36dc63fa66fa721612943ad
Guiltysparkz <coldwell.charles@gmail.com>
2024-12-10 11:45:41 +0100

    router issues

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,5 @@
 import React, { lazy } from "react";
-
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { HelmetMeta } from "./HelmetMeta";
 import { TheThemeProvider } from "../components/theme/ThemeProvider";
 import { CssBaseline } from "@mui/material";
@@ -11,20 +10,17 @@ import { Home } from "../pages/Home";
 const PageNotFound = lazy(() => import("../pages/PageNotFound"));
 
 export const App = () => {
-
-    return (
-      <TheThemeProvider>
-        <CssBaseline />
-        <Router>
-          <HelmetMeta />
-          <Switch>
-            <Router basename="/Charles-Coldwell-Portfolio">
-              <Route path="/" exact component={Home} />
-              {/* <Route path="/resume" component={Resume} /> */}
-              <Route path="*" component={PageNotFound} />
-            </Router>
-          </Switch>
-        </Router>
-      </TheThemeProvider>
-    );
+  return (
+    <TheThemeProvider>
+      <CssBaseline />
+      <Router basename="/Charles-Coldwell-Portfolio">
+        <HelmetMeta />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/resume" element={<Resume />} /> */}
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
+    </TheThemeProvider>
+  );
 };
